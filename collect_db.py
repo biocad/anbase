8e3a5858c911bc649b3c1dcd598903dfe7fd48d7
AlexKaneRUS <AlexKaneRUS@gmail.com>
2020-02-04 21:55:49 +0300

    something works

--- collect_db.py
+++ collect_db.py
@@ -27,6 +27,36 @@ DOT_PDB = '.pdb'
 DOT_FASTA = '.fasta'
 
 
+def get_while_true(curl):
+    not_finished = True
+
+    res = None
+
+    while not_finished:
+        try:
+            res = requests.get(curl)
+            not_finished = False
+        except Exception:
+            pass
+
+    return res.content.decode('utf-8')
+
+
+def post_while_true(url, json):
+    not_finished = True
+
+    res = None
+
+    while not_finished:
+        try:
+            res = requests.post(url, json)
+            not_finished &= False
+        except Exception:
+            pass
+
+    return res.content.decode('utf-8')
+
+
 class Complex:
     pdb_parser = PDBParser()
 
@@ -96,12 +126,12 @@ class Complex:
 
 def fetch_all_sequences(pdb_id):
     url = 'https://www.rcsb.org/pdb/download/downloadFastaFiles.do'
-    r = requests.post(url, {'structureIdList': pdb_id,
-                            'compressionType': 'uncompressed'})
+    r = post_while_true(url, {'structureIdList': pdb_id,
+                              'compressionType': 'uncompressed'})
 
     seqs = []
 
-    for line in r.content.decode('utf-8').split():
+    for line in r.split():
         if line.startswith('>'):
             seqs.append([line[5], ''])
         else:
@@ -116,7 +146,7 @@ def fetch_sequence(pdb_id, chain_id):
     return next(filter(lambda x: x[0] == chain_id, seqs))
 
 
-def get_bound_complexes(sabdab_summary_df):
+def get_bound_complexes(sabdab_summary_df, to_accept=None):
     def sub_nan(val):
         if isinstance(val, float) and math.isnan(val):
             return None
@@ -127,8 +157,10 @@ def get_bound_complexes(sabdab_summary_df):
     for _, row in sabdab_summary_df.iterrows():
         # if antigen's type is in lower case, it means that antigen is no good
         # for us, because it's a small molecule
-        if sub_nan(row[ANTIGEN_TYPE]) and row[ANTIGEN_TYPE].islower() and row[
-            PDB_ID] == '2vis':
+        if sub_nan(row[ANTIGEN_TYPE]) and row[ANTIGEN_TYPE].islower():
+            if to_accept and row[PDB_ID].upper() not in to_accept:
+                continue
+
             antigen_chains = row[ANTIGEN_CHAIN].split(' | ')
             complexes.append(Complex(
                 row[PDB_ID], sub_nan(row[H_CHAIN]), sub_nan(row[L_CHAIN]),
@@ -195,6 +227,22 @@ def load_bound_complexes(complexes, load_structures=False):
             print(comp.pdb_id, 'loaded')
 
 
+def compare_query_and_hit_seqs(query_seq, hit_seq):
+    cut_off_half = int(0.05 * len(query_seq) / 2)
+
+    c1 = query_seq in hit_seq
+    c2 = hit_seq in query_seq and abs(len(hit_seq) - len(query_seq)) \
+        <= cut_off_half * 2
+    c3 = query_seq[cut_off_half:-cut_off_half] in hit_seq
+    c4 = hit_seq[cut_off_half:-cut_off_half] in query_seq
+    c5 = query_seq[:-2 * cut_off_half] in hit_seq
+    c6 = hit_seq[:-2 * cut_off_half] in query_seq
+    c7 = query_seq[2 * cut_off_half:] in hit_seq
+    c8 = hit_seq[2 * cut_off_half:] in query_seq
+
+    return c1 or c2 or c3 or c4 or c5 or c6 or c7 or c8
+
+
 def is_match(query_seq, query_alignment, hit_alignment):
     if query_seq == hit_alignment:
         return True
@@ -209,7 +257,7 @@ def is_match(query_seq, query_alignment, hit_alignment):
     if '-' in hit_with_stripped_gaps:
         return False
 
-    return query_with_stripped_gaps == hit_with_stripped_gaps
+    return compare_query_and_hit_seqs(query_seq, hit_with_stripped_gaps)
 
 
 def get_blast_data(pdb_id, chain_id, seq):
@@ -217,8 +265,8 @@ def get_blast_data(pdb_id, chain_id, seq):
            '={}&chainId={}&eCutOff=10.0&matrix=BLOSUM62&outputFormat=XML'. \
         format(pdb_id, chain_id)
 
-    r = requests.get(curl)
-    xml = ElementTree.fromstring(r.content)
+    r = get_while_true(curl)
+    xml = ElementTree.fromstring(r)
 
     res = []
 
@@ -233,6 +281,10 @@ def get_blast_data(pdb_id, chain_id, seq):
                     hit_def_parts = hit_def.text.split('|')[0].split(':')
 
                     pdb_id = hit_def_parts[0]
+
+                    if pdb_id == '2W9D':
+                        print(pdb_id)
+
                     chain_ids = [x for x in hit_def_parts[2].split(',')]
 
                     for hsp in hit.find('Hit_hsps'):
@@ -247,10 +299,59 @@ def get_blast_data(pdb_id, chain_id, seq):
     return res
 
 
-def check_unbound(pdb_id, chain_seqs):
-    # don't work with duplicating chains
-    assert (len(chain_seqs) == len(frozenset(chain_seqs)))
+def retrieve_uniprot_ids(pdb_id):
+    url = 'https://www.uniprot.org/uploadlists/'
+    r = post_while_true(url, {'from': 'PDB_ID',
+                            'to': 'ACC',
+                            'format': 'tab',
+                            'query': pdb_id
+                            })
 
+    res = []
+
+    for line in r.split('\n')[1:-1]:
+        res.append(line.split('\t')[1])
+
+    return res
+
+
+def retrieve_names(pdb_id):
+    curl = 'https://www.rcsb.org/pdb/rest/describeMol?structureId={}' \
+        .format(pdb_id)
+
+    r = get_while_true(curl)
+    xml = ElementTree.fromstring(r)
+
+    res = []
+
+    for child in xml:
+        for polymer in child:
+            for attr in polymer:
+                if attr.tag == 'polymerDescription':
+                    res.append(attr.attrib['description'])
+
+    return res
+
+
+def check_names(names):
+    if len(list(frozenset(names))) == 1:
+        return True
+
+    split_names = list(map(lambda x: x.split(), names))
+
+    if len(list(frozenset(map(lambda x: len(x), split_names)))) != 1:
+        return False
+
+    common_set = set([])
+
+    for x in split_names:
+        for y in x:
+            common_set.add(y.upper())
+
+    return abs(len(list(common_set)) - len(split_names[0])) <= 1
+
+
+def check_unbound(pdb_id, chain_seqs):
     all_seqs_in_pdb = list(map(lambda x: x[1], fetch_all_sequences(pdb_id)))
 
     seqs_counts = []
@@ -258,17 +359,31 @@ def check_unbound(pdb_id, chain_seqs):
     for chain_seq in chain_seqs:
         seqs_counts.append(0)
         for seq in all_seqs_in_pdb:
-            if chain_seq == seq:
+            if compare_query_and_hit_seqs(chain_seq, seq):
                 seqs_counts[-1] += 1
 
+    # print(pdb_id)
+    # print(chain_seqs)
+    # print(all_seqs_in_pdb)
+    # print(list(map(lambda x: x > 0, seqs_counts)))
+    # print(len(retrieve_uniprot_ids(pdb_id)) == 1)
+
+    # we check that for every queried chain there is a matching chain in the
+    # given pdb and also we check that given pdb contains only one UniProt
+    # structure, what means that it contains only one structure, what means
+    # that structure is not in a complex, what means that it's unbound.
+    # also if names of all structures in pdb are different in no more than
+    # one word (for example, 'my ab heavy chain' and 'my ab light chain)
+    # it usually means that structures form one macromolecule,
+    # hence their complex is unbound
     return all(map(lambda x: x > 0, seqs_counts)) \
-        and all(map(lambda x: x == seqs_counts[0], seqs_counts)) \
-        and len(all_seqs_in_pdb) == sum(seqs_counts)
+           and (len(retrieve_uniprot_ids(pdb_id)) == 1
+                or check_names(retrieve_names(pdb_id)))
 
 
 def find_unbound_structure(pdb_id, chain_ids, seqs):
-    # TODO: SOMEHOW 2VIU is sorted out :C
-    
+    # TODO: идея — искать похожести по количеству совпадающих слов в названии макромолекулы
+
     # TODO: add memoization to find complexes more effectively
     candidates = [get_blast_data(pdb_id, chain_id, seq) for chain_id, seq in
                   zip(chain_ids, seqs)]
@@ -278,17 +393,21 @@ def find_unbound_structure(pdb_id, chain_ids, seqs):
                                            for
                                            candidate in candidates])
 
-    print(len(pdb_ids_in_intersection_prep))
     print(pdb_ids_in_intersection_prep)
 
     return list(
-        filter(lambda x: check_unbound(x, seqs), pdb_ids_in_intersection_prep))
+        filter(lambda x: x.upper() != pdb_id.upper()
+                         and check_unbound(x, seqs),
+               list(pdb_ids_in_intersection_prep)[:20]))
 
 
 def find_unbound_conformations(complex):
     unbound_antigen_valid_candidates = \
         find_unbound_structure(complex.pdb_id, complex.antigen_chains,
                                complex.antigen_seqs)
+
+    print(unbound_antigen_valid_candidates)
+
     unbound_antibody_valid_candidates = \
         find_unbound_structure(complex.pdb_id,
                                [complex.antibody_h_chain,
@@ -296,10 +415,7 @@ def find_unbound_conformations(complex):
                                [complex.antibody_h_seq,
                                 complex.antibody_l_seq])
 
-    print(unbound_antigen_valid_candidates)
-    print(unbound_antibody_valid_candidates)
-
-    return None
+    return unbound_antigen_valid_candidates, unbound_antibody_valid_candidates
 
 
 structures_summary = read_csv('data/sabdab_summary_all.tsv',
@@ -308,8 +424,163 @@ structures_summary = read_csv('data/sabdab_summary_all.tsv',
 # all_complexes = get_bound_complexes(structures_summary)
 # load_bound_complexes(all_complexes)
 
-comp = get_bound_complexes(structures_summary)[0]
-comp.load_structure()
-find_unbound_conformations(comp)
+test_structures = [('1AHW', '1FGN', '1TFH'),
+                   ('1BVK', '1BVL', '3LZT'),
+                   ('1DQJ', '1DQQ', '3LZT'),
+                   ('1E6J', '1E6O', '1A43'),
+                   ('1JPS', '1JPT', '1TFH'),
+                   ('1MLC', '1MLB', '3LZT'),
+                   ('1VFB', '1VFA', '8LYZ'),
+                   ('1WEJ', '1QBL', '1HRC'),
+                   ('2FD6', '2FAT', '1YWH'),
+                   ('2VIS', '1GIG', '2VIU'),
+                   ('2VXT', '2VXU', '1J0S'),
+                   ('2W9E', '2W9D', '1QM1'),
+                   ('3EOA', '3EO9', '3F74'),
+                   ('3HMX', '3HMW', '1F45'),
+                   ('3MXW', '3MXV', '3M1N'),
+                   ('3RVW', '3RVT', '3F5V'),
+                   ('4DN4', '4DN3', '1DOL'),
+                   ('4FQI', '4FQH', '2FK0'),
+                   ('4G6J', '4G5Z', 'H5N1'),
+                   ('4G6M', '4G6K', '4I1B'),
+                   ('4GXU', '4GXV', '4I1B')]
 
-# print(a)
+comps = get_bound_complexes(structures_summary,
+                            list(map(lambda x: x[0], test_structures)))
+
+for pdb_id, unbound_antibody_id, unbound_antigen_id in test_structures:
+    print('processing', pdb_id)
+
+    comp = list(filter(lambda x: x.pdb_id.upper() == pdb_id, comps))[0]
+    comp.load_structure()
+
+    unbound_antigen_candidates, unbound_antibody_candidates = \
+        find_unbound_conformations(comp)
+
+    print('antigen', 'expected:', unbound_antigen_id, 'got:',
+          unbound_antigen_candidates)
+    print('antibody', 'expected:', unbound_antibody_id, 'got:',
+          unbound_antibody_candidates)
+
+    if unbound_antigen_id not in unbound_antigen_candidates:
+        print('MISMATCH! in antigen')
+
+    if unbound_antibody_id not in unbound_antibody_candidates:
+        print('MISMATCH! in antibody')
+
+# Проблемы
+
+# 2W9E
+# antigen expected: 1QM1 got: ['1HJM', '1QM0', '2LSB', '4N9O', '6DU9', '5YJ5', '1QLX', '1I4M', '1QM3', '1HJN', '1QM2', '2IV5', '1QLZ', '1QM1', '4DGI']
+# antibody expected: 2W9D got: []
+# MISMATCH! in antibody
+#
+# 4DN4
+# antigen expected: 1DOL got: []
+# antibody expected: 4DN3 got: []
+# MISMATCH! in antigen
+# MISMATCH! in antibody
+#
+# 4G6M
+# antigen expected: 4I1B got: ['9ILB', '7I1B', '2KH2', '1TWM', '5BVP', '2NVH', '5I1B', '6I1B', '1TOO', '1IOB', '4I1B', '2I1B', '1I1B', '5MVZ', '4G6J']
+# antibody expected: 4G6K got: []
+# MISMATCH! in antibody
+#
+# 4GXU
+# antigen expected: 4I1B got: []
+# antibody expected: 4GXV got: []
+# MISMATCH! in antigen
+# MISMATCH! in antibody
+
+# Запуск 1
+
+# processing 4DN4
+# {'4DN4'}
+# []
+# 2W9D
+# 2W9D
+# {'4DN3', '2XTJ', '4DN4'}
+# antigen expected: 1DOL got: []
+# antibody expected: 4DN3 got: ['4DN3']
+# MISMATCH! in antigen
+# processing 4FQI
+# {'4FQI', '6PCX', '6PD6', '3GBM', '6PD5', '4MHH', '3ZP0', '6CFG', '6PD3', '6E3H', '6CF5', '2FK0', '6B3M', '3ZP1'}
+# ['6PCX', '6PD6', '3GBM', '6PD5', '3ZP0', '6CFG', '6PD3', '6CF5', '2FK0', '3ZP1']
+# 2W9D
+# 2W9D
+# {'6CNV', '5CJS', '4FQI', '4FQV', '4FQH', '4LLD', '4FQY', '5CJQ'}
+# antigen expected: 2FK0 got: ['6PCX', '6PD6', '3GBM', '6PD5', '3ZP0', '6CFG', '6PD3', '6CF5', '2FK0', '3ZP1']
+# antibody expected: 4FQH got: ['4FQV', '4FQH', '4FQY']
+# processing 4G6J
+# {'1I1B', '2KH2', '1ITB', '2I1B', '5MVZ', '4I1B', '7I1B', '3O4O', '9ILB', '4G6J', '4G6M', '1IOB', '5BVP', '5I1B', '2NVH', '4DEP', '6I1B'}
+# ['1I1B', '2KH2', '2I1B', '4I1B', '7I1B', '9ILB', '4G6M', '1IOB', '5BVP', '5I1B', '2NVH', '6I1B']
+# 2W9D
+# 2W9D
+# {'4G5Z', '2XTJ', '4G6J'}
+# antigen expected: H5N1 got: ['1I1B', '2KH2', '2I1B', '4I1B', '7I1B', '9ILB', '4G6M', '1IOB', '5BVP', '5I1B', '2NVH', '6I1B']
+# antibody expected: 4G5Z got: ['4G5Z']
+# MISMATCH! in antigen
+# processing 4G6M
+# {'1I1B', '2KH2', '1ITB', '2I1B', '1TOO', '5MVZ', '4I1B', '7I1B', '4G6J', '9ILB', '3O4O', '4G6M', '1TWM', '1IOB', '5BVP', '5I1B', '2NVH', '4DEP', '6I1B'}
+# ['1I1B', '2KH2', '2I1B', '1TOO', '5MVZ', '4I1B', '7I1B', '4G6J', '9ILB', '1TWM', '1IOB', '5BVP', '5I1B', '2NVH', '6I1B']
+# 2W9D
+# 2W9D
+# {'4G6K', '4G6M', '2XTJ'}
+# antigen expected: 4I1B got: ['1I1B', '2KH2', '2I1B', '1TOO', '5MVZ', '4I1B', '7I1B', '4G6J', '9ILB', '1TWM', '1IOB', '5BVP', '5I1B', '2NVH', '6I1B']
+# antibody expected: 4G6K got: ['4G6K']
+# processing 4GXU
+# {'3LZF', '1RUZ', '4PY8', '4EEF', '4GXU', '3R2X', '5C0R', '3GBN', '2WRG', '5C0S'}
+# ['3LZF', '1RUZ', '4PY8', '4EEF', '3R2X', '3GBN', '2WRG']
+# 2W9D
+# 2W9D
+# {'4LLD', '4GXU', '4GXV'}
+# antigen expected: 4I1B got: ['3LZF', '1RUZ', '4PY8', '4EEF', '3R2X', '3GBN', '2WRG']
+# antibody expected: 4GXV got: ['4GXV']
+# MISMATCH! in antigen
+
+# Запуск 2
+
+# processing 4DN4
+# {'4DN4'}
+# []
+# 2W9D
+# 2W9D
+# {'2XTJ', '4DN4', '4DN3'}
+# antigen expected: 1DOL got: []
+# antibody expected: 4DN3 got: ['4DN3']
+# MISMATCH! in antigen
+# processing 4FQI
+# {'6PCX', '6PD5', '2FK0', '6PD6', '3ZP1', '6CF5', '6PD3', '4MHH', '6B3M', '6E3H', '4FQI', '6CFG', '3GBM', '3ZP0'}
+# ['6PCX', '6PD5', '2FK0', '6PD6', '3ZP1', '6CF5', '6PD3', '6CFG', '3GBM', '3ZP0']
+# 2W9D
+# 2W9D
+# {'4FQY', '4FQH', '6CNV', '5CJS', '4FQV', '4FQI', '4LLD', '5CJQ'}
+# antigen expected: 2FK0 got: ['6PCX', '6PD5', '2FK0', '6PD6', '3ZP1', '6CF5', '6PD3', '6CFG', '3GBM', '3ZP0']
+# antibody expected: 4FQH got: ['4FQY', '4FQH', '4FQV']
+# processing 4G6J
+# {'5I1B', '5BVP', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '5MVZ', '4G6M', '4G6J', '1ITB', '4I1B', '4DEP', '2NVH', '1I1B', '3O4O', '9ILB'}
+# ['5I1B', '5BVP', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '4G6M', '4I1B', '2NVH', '1I1B', '9ILB']
+# 2W9D
+# 2W9D
+# {'2XTJ', '4G6J', '4G5Z'}
+# antigen expected: H5N1 got: ['5I1B', '5BVP', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '4G6M', '4I1B', '2NVH', '1I1B', '9ILB']
+# antibody expected: 4G5Z got: ['4G5Z']
+# MISMATCH! in antigen
+# processing 4G6M
+# {'1TWM', '5I1B', '5BVP', '1TOO', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '5MVZ', '4G6M', '4G6J', '1ITB', '4I1B', '4DEP', '2NVH', '1I1B', '3O4O', '9ILB'}
+# ['1TWM', '5I1B', '5BVP', '1TOO', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '5MVZ', '4G6J', '4I1B', '2NVH', '1I1B', '9ILB']
+# 2W9D
+# 2W9D
+# {'2XTJ', '4G6K', '4G6M'}
+# antigen expected: 4I1B got: ['1TWM', '5I1B', '5BVP', '1TOO', '2KH2', '2I1B', '1IOB', '7I1B', '6I1B', '5MVZ', '4G6J', '4I1B', '2NVH', '1I1B', '9ILB']
+# antibody expected: 4G6K got: ['4G6K']
+# processing 4GXU
+# {'3GBN', '3LZF', '1RUZ', '5C0R', '2WRG', '5C0S', '4GXU', '3R2X', '4EEF', '4PY8'}
+# ['3GBN', '3LZF', '1RUZ', '2WRG', '3R2X', '4EEF', '4PY8']
+# 2W9D
+# 2W9D
+# {'4GXU', '4LLD', '4GXV'}
+# antigen expected: 4I1B got: ['3GBN', '3LZF', '1RUZ', '2WRG', '3R2X', '4EEF', '4PY8']
+# antibody expected: 4GXV got: ['4GXV']
+# MISMATCH! in antigen
