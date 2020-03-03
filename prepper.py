04b350d386baf28b54c01a2dd53c0999bef05abb
AlexKaneRUS <AlexKaneRUS@gmail.com>
2020-03-03 14:39:15 +0300

    pdbfixer added

--- prepper.py
+++ prepper.py
@@ -3,15 +3,20 @@ import shutil
 import subprocess
 import time
 
-from collect_db import DB_PATH, DOT_PDB
-from post_hetatms_and_mols_elimination import HETATMS_DELETED
+DB_PATH = 'data'
+DOT_PDB = '.pdb'
+
+HETATMS_DELETED = 'hetatms_deleted'
 
 PDB_PREP_DIR = 'pdb_prep'
 SCHROD_SCRIPT_PATH = 'schrod_multi_prepare.sh'
 
-SCHROD_PREP_SUFF = '.o.pdb'
+PREP_SUFF = '.o.pdb'
 
 PREPPED = 'prepared'
+
+SCHROD = 'schrod'
+PDB_FIXER = 'PDBFixer'
 
 
 def get_pdb_paths(dir_path, prev_epoch):
@@ -25,42 +30,77 @@ def get_pdb_paths(dir_path, prev_epoch):
     return pdb_paths
 
 
-def schrod_prep(file_names):
-    expected_files = set(map(lambda x: x + SCHROD_PREP_SUFF, file_names))
-
-    shutil.copyfile(SCHROD_SCRIPT_PATH,
-                    os.path.join(PDB_PREP_DIR, SCHROD_SCRIPT_PATH))
-
-    os.chdir(PDB_PREP_DIR)
-    subprocess.run(['bash', SCHROD_SCRIPT_PATH], stdout=subprocess.PIPE)
-
+def await_expected_files(expected_files, tmp_dir):
     res = {}
 
     while len(expected_files) > 0:
         print(expected_files)
         for path in os.listdir('.'):
-            if path.endswith(SCHROD_PREP_SUFF) and path in expected_files:
+            if path.endswith(PREP_SUFF) and path in expected_files:
                 expected_files.remove(path)
-                name = path[:-len(SCHROD_PREP_SUFF)]
+                name = path[:-len(PREP_SUFF)]
                 print(name)
-                res[name] = os.path.join(PDB_PREP_DIR, path)
+                res[name] = os.path.join(tmp_dir, path)
         time.sleep(1)
+
+    return res
+
+
+def schrod_prep(file_names, tmp_dir):
+    expected_files = set(map(lambda x: x + PREP_SUFF, file_names))
+
+    shutil.copyfile(SCHROD_SCRIPT_PATH,
+                    os.path.join(tmp_dir, SCHROD_SCRIPT_PATH))
+
+    os.chdir(tmp_dir)
+    subprocess.run(['bash', SCHROD_SCRIPT_PATH], stdout=subprocess.PIPE)
+
+    res = await_expected_files(expected_files, tmp_dir)
 
     os.chdir('..')
 
     return res
 
 
-def prep_pdbs(last_epoch_name, epoch_name):
+def pdb_fixer_prep(file_names, tmp_dir):
+    expected_files = set(map(lambda x: x + PREP_SUFF, file_names))
+
+    os.chdir(tmp_dir)
+
+    running_ps = set()
+
+    for name in file_names:
+        if len(running_ps) >= 50:
+            while len(running_ps) > 0:
+                for p in list(running_ps):
+                    if p.returncode is not None:
+                        running_ps.remove(p)
+
+                time.sleep(1)
+
+        p = subprocess.Popen(
+            ['pdbfixer', name, '--replace-nonstandard', '--add-residues',
+             '--output={}{}'.format(name, PREP_SUFF)], stdout=subprocess.PIPE)
+
+        running_ps.add(p)
+
+    res = await_expected_files(expected_files, tmp_dir)
+
+    os.chdir('..')
+
+    return res
+
+
+def prep_pdbs(last_epoch_name, epoch_name, db_path, mode, tmp_dir):
     pdb_paths = []
 
-    for file in os.listdir(DB_PATH):
-        dir_path = os.path.join(DB_PATH, file)
+    for file in os.listdir(db_path):
+        dir_path = os.path.join(db_path, file)
         if os.path.isdir(dir_path):
             pdb_paths += get_pdb_paths(dir_path, last_epoch_name)
 
-    if not os.path.exists(PDB_PREP_DIR):
-        os.mkdir(PDB_PREP_DIR)
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
 
     path_to_name = {}
 
@@ -74,9 +114,10 @@ def prep_pdbs(last_epoch_name, epoch_name):
         path_to_name[path] = name
         names.append(name)
 
-        shutil.copyfile(path, os.path.join(PDB_PREP_DIR, name))
+        shutil.copyfile(path, os.path.join(tmp_dir, name))
 
-    path_to_prepped_schrod = schrod_prep(names)
+    path_to_prepped_schrod = schrod_prep(names, tmp_dir) if mode == SCHROD \
+        else pdb_fixer_prep(names, tmp_dir)
 
     unprepped_paths = []
 
@@ -100,4 +141,29 @@ def prep_pdbs(last_epoch_name, epoch_name):
 
 
 if __name__ == '__main__':
-    prep_pdbs(HETATMS_DELETED, PREPPED)
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option('--db', default=DB_PATH, dest='db', metavar='DB',
+                      help='Path to database [default: {}]'.format(DB_PATH))
+    parser.add_option('--mode', default=SCHROD, dest='mode', metavar='MODE',
+                      help='{} for preparation using schrodinger. '
+                           '{} for preparation using PDBFixer. [default: {}]'.
+                      format(SCHROD, PDB_FIXER, SCHROD))
+    parser.add_option('--prev-epoch', default=HETATMS_DELETED,
+                      dest='prev_epoch', metavar='PREV_EPOCH',
+                      help='Name of the epoch structures from which will be '
+                           'prepared. [default: {}]'.format(HETATMS_DELETED))
+    parser.add_option('--tmp-dir', default=PDB_PREP_DIR,
+                      dest='tmp_dir', metavar='TMP_DIR',
+                      help='Directory in which preparations will take place. '
+                           'If it doesn\'t exist, it will be created '
+                           'automatically.[default: {}]'.format(PDB_PREP_DIR))
+    parser.add_option('--cur-epoch', default=PREPPED,
+                      dest='cur_epoch', metavar='CUR_EPOCH',
+                      help='Name of the preparation epoch. [default: {}]'.
+                      format(PREPPED))
+    options, _ = parser.parse_args()
+
+    prep_pdbs(options.prev_epoch, options.cur_epoch, options.db, options.mode,
+              options.tmp_dir)
