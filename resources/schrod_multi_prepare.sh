566845631d0451322ed5b89b8ce5e50f32e68f9c
AlexKaneRUS <AlexKaneRUS@gmail.com>
2020-03-04 18:04:57 +0300

    updated schrod runner

--- resources/schrod_multi_prepare.sh
+++ resources/schrod_multi_prepare.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 files=(*.pdb)
+maxtasks=50
 ix=0
-iy=0
 len=${#files[@]}
-while [[ $ix -le $len ]]
+while [[ $ix -lt $len ]]
 do
-  let 'iy = ix + 50'
   ctime=$(date)
   echo "[$ctime] Started iteration $ix" >> LOG.txt
-  curbatch=${files[@]:$ix:50}
-  for f in  $curbatch
+  tasks=$($SCHRODINGER/jobcontrol -list | wc -l)
+  while [[ $tasks -le $maxtasks ]] && [[ $ix -lt $len ]]
   do
-    $SCHRODINGER/utilities/prepwizard $(
-        echo "-disulfides"          # build the disulfide bridges
-        echo "-fillloops"           # fill missing residues
-        echo "-fillsidechains"      # fill side-chains where necessary
-        echo "-mse"                 # convert selenomethionine residues to methionine
-        echo "-noepik"              # turn off epik, since it's used for small molecules and we don't have ones
-        echo "-noimpref"            # IMPORTANT: turn off minimization
-        echo "-rehtreat"            # IMPORTANT: delete existing hydrogen atoms and build new ones
+      f=${files[ix]}
+      $SCHRODINGER/utilities/prepwizard $(
+        echo "-disulfides"                      # build the disulfide bridges
+        echo "-fillloops"                       # fill missing residues
+        echo "-fillsidechains"                  # fill side-chains where necessary
+        echo "-mse"                             # convert selenomethionine residues to methionine
+        echo "-noepik"                          # turn off epik, since it's used for small molecules and we don't have ones
+        echo "-noimpref"                        # IMPORTANT: turn off minimization
+        echo "-rehtreat"                        # IMPORTANT: delete existing hydrogen atoms and build new ones
         echo "-fasta_file $(realpath $f).fasta" # fasta file which is used to fill missing residues. IMPORTANT: has to be absolute
         echo "$f"
         echo "$f.o.pdb"
-    )
-  done
-  let 'ix += 50'
-  sleep 5
-  tasks=$($SCHRODINGER/jobcontrol -list | wc -l)
-  while [[ $tasks -gt "1" ]]
-  do
-    sleep 5
-    ctime=$(date)
-    echo "[$ctime] Waiting for $tasks tasks to terminate. Iteration $ix" >> LOG.txt
-    tasks=$($SCHRODINGER/jobcontrol -list | wc -l)
+      )
+      let "ix += 1"
+      sleep 0.1
+      tasks=$($SCHRODINGER/jobcontrol -list | wc -l)
   done
 done
