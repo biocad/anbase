41da20112d841defc5ad71619022051a45f40c6e
AlexKaneRUS <AlexKaneRUS@gmail.com>
2020-03-04 19:13:10 +0300

    updated pdbfixer

--- resources/pdbfixer_multi_prepare.sh
+++ resources/pdbfixer_multi_prepare.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 files=(*.pdb)
-conda activate openmm
-for f in $files
+for f in "${files[@]}"
 do
     echo "Prepping: $f" >> LOG.txt
-	pdbfixer $f --replace-nonstandard --add-residues --output="$f.o.pdb"
+	$PDBFIXER $f --replace-nonstandard --add-residues --output="$f.o.pdb"
 	echo "Prepped: $f" >> LOG.txt
 done
