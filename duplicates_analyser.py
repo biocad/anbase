6f3862aa47b2695603dbc89616d39682eca05980
AlexKaneRUS <AlexKaneRUS@gmail.com>
2020-03-15 13:50:35 +0300

    duplicates with annotations

--- duplicates_analyser.py
+++ duplicates_analyser.py
@@ -9,10 +9,13 @@ DB_INFO_PATH = 'db_info.csv'
 DB_PATH = 'data'
 
 SEQS = 'seqs'
+ANNOTATION = 'annotation'
 
 DOT_FASTA = '.fasta'
 
 DUPLICATES_CSV = 'duplicates.csv'
+
+CDRS = ['CDR1', 'CDR2', 'CDR3']
 
 
 class CandidateInfo:
@@ -32,6 +35,23 @@ class CandidateInfo:
 
         self.ab_seqs = []
         self.ag_seqs = []
+
+        self.ab_cdrs_annotation_b = []
+
+    def load_ab_annotation(self, db_path):
+        comp_path = os.path.join(db_path,
+                                 comp_name_to_dir_name(self.comp_name))
+        ab_fasta_b = read_annotation(
+            os.path.join(os.path.join(comp_path, ANNOTATION), self.pdb_id_b +
+                         DOT_FASTA))
+
+        for x in self.ab_chain_ids_b:
+            annotation = {}
+
+            for cdr in CDRS:
+                annotation[cdr] = ab_fasta_b[(x, cdr)]
+
+            self.ab_cdrs_annotation_b.append(annotation)
 
     def load_sequences(self, db_path):
         comp_path = os.path.join(db_path,
@@ -62,7 +82,22 @@ def read_fasta(path):
     return res
 
 
-def similarity_of_two_seqs(seq1, seq2):
+def read_annotation(path):
+    res = {}
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+        i = 0
+        while i < len(lines):
+            [chain_id, region] = lines[i].strip()[1:].split(':')
+            res[(chain_id, region)] = lines[i + 1].strip()
+            i += 2
+
+    return res
+
+
+def calc_matches_mismatches(seq1, seq2):
     alignment_list = pairwise2.align.localxs(seq1, seq2, -1, -1,
                                              penalize_end_gaps=False,
                                              one_alignment_only=True)
@@ -81,9 +116,30 @@ def similarity_of_two_seqs(seq1, seq2):
         if query_alignment[i] == target_alignment[i]:
             matches_count += 1
 
-    score = float(matches_count) / float(len(query_alignment))
+    return matches_count, len(query_alignment) - matches_count
+
+
+def similarity_of_two_seqs(seq1, seq2):
+    matches, mismatches = calc_matches_mismatches(seq1, seq2)
+
+    score = float(matches) / float(matches + mismatches)
 
     return score >= 0.9
+
+
+def similarity_of_abs(comp1, comp2):
+    for cdr in CDRS:
+        seq1 = comp1.ab_cdrs_annotation_b[cdr]
+        seq2 = comp2.ab_cdrs_annotation_b[cdr]
+
+        _, mismatches = calc_matches_mismatches(
+            comp1.ab_cdrs_annotation_b[cdr], comp2.ab_cdrs_annotation_b[cdr])
+
+        if mismatches >= 2:
+            print('Not equal:', seq1, 'and', seq2, ', mismatches:', mismatches,
+                  flush=True)
+            return False
+    return True
 
 
 def similarity_of_two_complexes(comp1, comp2):
@@ -91,9 +147,11 @@ def similarity_of_two_complexes(comp1, comp2):
             len(comp1.ag_seqs) != len(comp2.ag_seqs):
         return False
 
-    return all(map(lambda p: similarity_of_two_seqs(p[0], p[1]),
-                   zip(comp1.ab_seqs + comp1.ag_seqs,
-                       comp2.ab_seqs + comp2.ag_seqs)))
+    ab_chains_similar = similarity_of_abs(comp1, comp2)
+    ag_chains_similar = all(map(lambda p: similarity_of_two_seqs(p[0], p[1]),
+                                zip(comp1.ag_seqs, comp2.ag_seqs)))
+
+    return ab_chains_similar and ag_chains_similar
 
 
 if __name__ == '__main__':
